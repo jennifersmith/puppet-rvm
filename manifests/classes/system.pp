6147ee1203bbddd979c946bd27edebd399c0cf1e
Jennifer Smith <jennifer@home>
2011-12-04 12:35:39 -0600

    Now when installing RVM, we pass in a home directory instead of a user

--- manifests/classes/system.pp
+++ manifests/classes/system.pp
@@ -1,10 +1,10 @@
-class rvm::system($version='latest',$user) {    
+class rvm::system($version='latest',$homedir) {    
    include rvm::dependencies
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm -o rvm-installer ; chmod +x rvm-installer ; ./rvm-installer'",
-    creates => "/Users/${user}/.rvm",
+    creates => "${homedir}/.rvm",
     require => [
       Class['rvm::dependencies'],
     ],
